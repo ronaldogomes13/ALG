64d31b1d0934f0460a99819edd9dbcfe42fc70ac
ronaldogomes <ronaldogomes13@gmail.com>
2015-11-10 22:23:36 -0200

    Trabalho de Compiladores - Etapa 1

--- src/analise/Token.java
+++ src/analise/Token.java
@@ -1,6 +1,7 @@
 package analise;
 
 public enum Token {
+    //Tirar o token erro desta enum pois ele tem informações diferentes a serem guadadas
 	NUM(1), LITERAL(2),ID(3),COMENTARIO(4), EOF(5),OPR(6),RCB(7),OPM(8),AB_P(9),FC_P(10),PT_V(11),ERRO(12);
 	int valor=0;
 	String tipo;
